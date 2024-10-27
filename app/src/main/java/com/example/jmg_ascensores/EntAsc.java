1e20db225817a41fcbcb07c56ac325a8c787af10
Rodrigo0428 <rodrigocarrasco2004@gmail.com>
2024-10-26 23:53:59 -0500

    Informacion ascensores

--- app/src/main/java/com/example/jmg_ascensores/EntAsc.java
+++ app/src/main/java/com/example/jmg_ascensores/EntAsc.java
@@ -1,35 +1,38 @@
 package com.example.jmg_ascensores;
 
-import java.util.Date;
-
 public class EntAsc {
 
-    private String model;
+    private String modelo; // Atributo
     private String marca;
     private Integer codAsc;
 
+    // Getter para 'modelo'
     public String getModel() {
-        return model;
+        return modelo; // Asegúrate de que 'modelo' esté correctamente declarado
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    // Setter para 'modelo'
+    public void setModelo(String modelo) {
+        this.modelo = modelo; // Asigna el valor al atributo 'modelo'
     }
 
+    // Getter para 'marca'
     public String getMarca() {
         return marca;
     }
 
+    // Setter para 'marca'
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    // Getter para 'codAsc'
     public Integer getCodAsc() {
         return codAsc;
     }
 
+    // Setter para 'codAsc'
     public void setCodAsc(Integer codAsc) {
         this.codAsc = codAsc;
     }
 }
-
