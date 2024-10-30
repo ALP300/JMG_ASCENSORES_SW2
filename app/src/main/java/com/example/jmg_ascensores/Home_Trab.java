0d414f055613b2a1926c2f573eacc9011d9d6b35
ALP300 <aitorpalomino1@gmail.com>
2024-10-30 09:55:20 -0500

    Vista administrador

--- app/src/main/java/com/example/jmg_ascensores/Home_Trab.java
+++ app/src/main/java/com/example/jmg_ascensores/Home_Trab.java
@@ -4,40 +4,40 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home_Trab extends AppCompatActivity {
-    private Button btnEmpresasAsignadas;
-    private Button btnCerrarSesion;
+    private LinearLayout btnEmpresasAsignadas;
+    private LinearLayout btnCerrarSesion;
     private String codTrab;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_trabajador);
 
-        btnEmpresasAsignadas = findViewById(R.id.idEmpresa);
-        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        btnEmpresasAsignadas = findViewById(R.id.menu_empresa);
+        btnCerrarSesion = findViewById(R.id.menu_cerrar_sesion); // Corregir ID
+
         codTrab = getIntent().getStringExtra("codTrab");
 
         btnEmpresasAsignadas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Al hacer clic en el botón ADMINISTRADOR, iniciar MainActivity
-
+                // Iniciar View_TareasEmpresa al hacer clic en "Empresas Asignadas"
                 Intent intent = new Intent(Home_Trab.this, View_TareasEmpresa.class);
                 intent.putExtra("codTrab", codTrab);
                 startActivity(intent);
-                finish(); // Cerrar OpcionesActivity
+                finish(); // Cerrar Home_Trab
             }
         });
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 // Redirigir a la actividad de inicio de sesión
                 Intent intent = new Intent(Home_Trab.this, Home_Main.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Limpiar pila de actividades
@@ -45,6 +45,5 @@ public class Home_Trab extends AppCompatActivity {
                 finish();  // Cerrar la actividad actual
             }
         });
-
-
-}}
+    }
+}
