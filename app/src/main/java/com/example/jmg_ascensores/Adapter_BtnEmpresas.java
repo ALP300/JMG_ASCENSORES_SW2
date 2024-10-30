2af0c73abc4192410236c7597248d7e6f20f5e26
USMPDNBJ <david_nolasco@usmp.pe>
2024-10-30 10:29:57 -0500

    correcion

---
+++ app/src/main/java/com/example/jmg_ascensores/Adapter_BtnEmpresas.java
@@ -0,0 +1,65 @@
+package com.example.jmg_ascensores;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Adapter_BtnEmpresas extends BaseAdapter {
+
+    private Context context;
+    private List<Ent_Cliente> cliente;
+
+    public Adapter_BtnEmpresas(Context context, List<Ent_Cliente> cliente) {
+        this.context = context;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public int getCount() {
+        return cliente.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return cliente.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.adapter_empresas, parent, false);
+        }
+
+        // Obtener el nombre de la empresa y darle formato
+        String empresa = cliente.get(position).getNombre_empresa();
+        empresa = "Empresa " + empresa.substring(0, 1).toUpperCase() + empresa.substring(1);
+
+        // Configurar el TextView con el nombre de la empresa
+        TextView empresaTextView = convertView.findViewById(R.id.empresaTextView);
+        empresaTextView.setText(empresa);
+
+        // Configurar el click en toda la tarjeta para abrir la actividad
+        String finalEmpresa = empresa;
+        convertView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, View_TrabajadorTarea.class);
+            intent.putExtra("empresa", finalEmpresa);
+            intent.putExtra("ubicacion", cliente.get(position).getUbicacion());
+            context.startActivity(intent);
+        });
+
+        return convertView;
+    }
+}
